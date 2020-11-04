9fce62517df5c33a2148409957a5841331e9b314
GalatiMarco <73830862+GalatiMarco@users.noreply.github.com>
2020-11-04 17:38:02 +0100

    maledizione

--- CinemaNapoli/src/controller/GestisciDatabase.java
+++ CinemaNapoli/src/controller/GestisciDatabase.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,146 +12,186 @@ import javax.servlet.http.HttpServlet;
 import model.*;
 
 
-@WebServlet("/GestisciDatabase")
-public class GestisciDatabase extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-	private EntityManagerFactory emf;
-	private EntityManager em;
-	
-    public GestisciDatabase() {
-        super();
-        emf = Persistence.createEntityManagerFactory("CinemaNapoli");
-        em = emf.createEntityManager();
-    }
+public class GestisciDatabase {
+
+	private static EntityManager getManager() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		}catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("CinemaNapoli"); //connetto il database
+        EntityManager em  = emf.createEntityManager();
+        return em;
+	}
+
     
     //------METODI AGGIUNGI
-	public void aggiungiUser(User u) {
-		em.getTransaction().begin();
+	public static void aggiungiUser(User u) {
+		EntityManager em=getManager();
+		EntityTransaction et=em.getTransaction();
+		et.begin();
 		em.persist(u);
-		em.getTransaction().commit();
+		et.commit();
 	}
 	
-	public void aggiungiSala(Sala s) {
-		em.getTransaction().begin();
+	public static void aggiungiSala(Sala s) {
+		EntityManager em=getManager();
+		EntityTransaction et=em.getTransaction();
+		et.begin();
 		em.persist(s);
-		em.getTransaction().commit();
+		et.commit();
 	}
 	
-	public void aggiungiFilm(Film f) {
-		em.getTransaction().begin();
+	public static void aggiungiFilm(Film f) {
+		EntityManager em=getManager();
+		EntityTransaction et=em.getTransaction();
+		et.begin();
 		em.persist(f);
-		em.getTransaction().commit();
+		et.commit();
 	}
 	
-	public void aggiungiProiezione(Proiezione p) {
-		em.getTransaction().begin();
+	public static void aggiungiProiezione(Proiezione p) {
+		EntityManager em=getManager();
+		EntityTransaction et=em.getTransaction();
+		et.begin();
 		em.persist(p);
-		em.getTransaction().commit();
+		et.commit();
 	}
 	
-	public void aggiungiPrenotazione(Prenotazione p) {
-		em.getTransaction().begin();
+	public static void aggiungiPrenotazione(Prenotazione p) {
+		EntityManager em=getManager();
+		EntityTransaction et=em.getTransaction();
+		et.begin();
 		em.persist(p);
-		em.getTransaction().commit();
+		et.commit();
 	}
 	  
 	//----- METODI LEGGI
-	public List<User> leggiUser() {
+	public static List<User> leggiUser() {
+		EntityManager em=getManager();
 		return em.createNamedQuery("User.findAll").getResultList();
 		
 	}
 	
-	public List <Sala> leggiSala() {
+	public static List <Sala> leggiSala() {
+		EntityManager em=getManager();
 		return em.createNamedQuery("Sala.findAll").getResultList();
 	}
 	
-	public List <Film> leggiFilm() {
+	public static List <Film> leggiFilm() {
+		EntityManager em=getManager();
 		return em.createNamedQuery("Film.findAll").getResultList();
 	}
 	
-	public List <Proiezione> leggiProiezione() {
+	public static List <Proiezione> leggiProiezione() {
+		EntityManager em=getManager();
 		return em.createNamedQuery("Proiezione.findAll").getResultList();
 	}
 	
-	public List <Prenotazione> leggiPrenotazione() {
+	public static List <Prenotazione> leggiPrenotazione() {
+		EntityManager em=getManager();
 		return em.createNamedQuery("Prenotazione.findAll").getResultList();
 	}
 	
 	//------METODI MODIFICA
-	public void modificaUser(User u) {
-		em.getTransaction().begin();
+	public static void modificaUser(User u) {
+		EntityManager em=getManager();
+		EntityTransaction et=em.getTransaction();
+		et.begin();
 		em.merge(u);
-		em.getTransaction().commit(); 
+		et.commit(); 
 	}
 	
-	public void modificaSala(Sala s) {
-		em.getTransaction().begin();
+	public static void modificaSala(Sala s) {
+		EntityManager em=getManager();
+		EntityTransaction et=em.getTransaction();
+		et.begin();
 		em.merge(s);
-		em.getTransaction().commit();
+		et.commit();
 	}
 	 
-	public void modificaFilm(Film f) {
-		em.getTransaction().begin();
+	public static void modificaFilm(Film f) {
+		EntityManager em=getManager();
+		EntityTransaction et=em.getTransaction();
+		et.begin();
 		em.merge(f);
-		em.getTransaction().commit();
+		et.commit();
 	}
 	
-	public void modificaProiezione(Proiezione p) {
-		em.getTransaction().begin();
+	public static void modificaProiezione(Proiezione p) {
+		EntityManager em=getManager();
+		EntityTransaction et=em.getTransaction();
+		et.begin();
 		em.merge(p);
-		em.getTransaction().commit();
+		et.commit();
 	}
 	
-	public void modificaPrenotazione(Prenotazione p) {
-		em.getTransaction().begin();
+	public static void modificaPrenotazione(Prenotazione p) {
+		EntityManager em=getManager();
+		EntityTransaction et=em.getTransaction();
+		et.begin();
 		em.merge(p);
-		em.getTransaction().commit();
+		et.commit();
 	}
 	
 	//-----METODO ELIMINA
-	public void annullaPrenotazione(Prenotazione p) {
-		em.getTransaction().begin();
+	public static void annullaPrenotazione(Prenotazione p) {
+		EntityManager em=getManager();
+		EntityTransaction et=em.getTransaction();
+		et.begin();
 		em.remove(p);
-		em.getTransaction().commit();
+		et.commit();
 	}
 	
 	//-----METODI RICERCA SPECIFICA
-	public User cercaUser(long id) {
-		em.getTransaction().begin();
+	public static User cercaUser(long id) {
+		EntityManager em=getManager();
+		EntityTransaction et=em.getTransaction();
+		et.begin();
 		User u=em.find(User.class, id);
-		em.getTransaction().commit();
+		et.commit();
 		return u;
 	}
 	
-	public User cercaUserByUsername(String username) {
+	public static User cercaUserByUsername(String username) {
+		EntityManager em=getManager();
 		return em.createQuery("SELECT u FROM User u WHERE u.username='"+username+"'",User.class).getSingleResult();
 	}
 	
-	public Film cercaFilm(long id) {
-		em.getTransaction().begin();
+	public static Film cercaFilm(long id) {
+		EntityManager em=getManager();
+		EntityTransaction et=em.getTransaction();
+		et.begin();
 		Film f=em.find(Film.class, id);
-		em.getTransaction().commit();
+		et.commit();
 		return f;
 	}
 	
-	public Sala cercaSala(long id) {
-		em.getTransaction().begin();
+	public static Sala cercaSala(long id) {
+		EntityManager em=getManager();
+		EntityTransaction et=em.getTransaction();
+		et.begin();
 		Sala s=em.find(Sala.class, id);
-		em.getTransaction().commit();
+		et.commit();
 		return s;
 	}
 	
-	public Proiezione cercaProiezione(long id) {
-		em.getTransaction().begin();
+	public static Proiezione cercaProiezione(long id) {
+		EntityManager em=getManager();
+		EntityTransaction et=em.getTransaction();
+		et.begin();
 		Proiezione p=em.find(Proiezione.class, id);
-		em.getTransaction().commit();
+		et.commit();
 		return p;
 	}
 	
-	public Prenotazione cercaPrenotazione(long id) {
-		em.getTransaction().begin();
+	public static Prenotazione cercaPrenotazione(long id) {
+		EntityManager em=getManager();
+		EntityTransaction et=em.getTransaction();
+		et.begin();
 		Prenotazione p=em.find(Prenotazione.class, id);
-		em.getTransaction().commit();
+		et.commit();
 		return p;
 	}
 }
