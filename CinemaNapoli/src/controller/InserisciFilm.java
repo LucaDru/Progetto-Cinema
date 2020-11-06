089783c923582776cd8b5b999852e86efd9d7979
MatteoSavignano <73559634+MatteoSavignano@users.noreply.github.com>
2020-11-06 10:47:32 +0100

    Servlet e jsp inserisciFilm

--- CinemaNapoli/src/controller/InserisciFilm.java
+++ CinemaNapoli/src/controller/InserisciFilm.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpSession;
 import model.Film;
 import static util.GestisciDatabase.*;
 
-@WebServlet("/InserisciFilm")
+@WebServlet(name="inserisciFilm", urlPatterns={"/InserisciFilm"})
 public class InserisciFilm extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	List<Film>listaFilm;
@@ -42,19 +42,21 @@ public class InserisciFilm extends HttpServlet {
 				f.setDurata(Integer.parseInt(request.getParameter("durata")));
 				f.setAnnouscita(Integer.parseInt(request.getParameter("annouscita")));
 				List<Film>listaFilm=leggiFilm();
+				boolean possoinserire = true;
 				for(Film film:listaFilm) {
-					if(!film.getTitolo().equalsIgnoreCase(f.getTitolo()) && film.getAnnouscita()!=(f.getAnnouscita())) {
-						aggiungiFilm(f);
-						
-					
+					if(film.getTitolo().equalsIgnoreCase(f.getTitolo()) && film.getAnnouscita()==(f.getAnnouscita())) {
+						System.out.println("Il film già c'è");
+						possoinserire=false;
+						break;				
 					}
 				}
+				if(possoinserire) {
+					aggiungiFilm(f);
+				}
 			}
-			
-		
-		
-		
+				
 		doGet(request, response);
 	}
 
 }
+
